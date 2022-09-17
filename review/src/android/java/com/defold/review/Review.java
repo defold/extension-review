e2ad049132a8f2df3a311e57290bb51df8bdcbe2
Mike <skaterdadsoftware@gmail.com>
2022-09-17 14:58:33 -0500

    Use com.google.android.gms.tasks

--- review/src/android/java/com/defold/review/Review.java
+++ review/src/android/java/com/defold/review/Review.java
@@ -6,8 +6,8 @@ import android.os.Build;
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
-import com.google.android.play.core.tasks.Task;
-import com.google.android.play.core.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
 
 public class Review {
 
